f7ad99fed6250234ad593066c1216eaeb580b9a9
mohamed <mohamedomer2582004@gmail.com>
2024-09-11 07:52:56 +0300

    simple modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const NotesApp());
-}
+void main() => runApp(const NotesApp());
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
