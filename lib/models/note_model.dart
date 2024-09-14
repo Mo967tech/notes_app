3d75077d80c3f7fb775ed02da0c6e63e462ec77e
mohamed <mohamedomer2582004@gmail.com>
2024-09-14 14:18:12 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,11 @@
+class NoteModel {
+  final String title, content, date;
+  final int color;
+  
+  const NoteModel({
+    required this.title,
+    required this.content,
+    required this.date,
+    required this.color,
+  });
+}
