558bc12bd8f2c693e71ed981a8989c8ceba41e8e
mohamed <mohamedomer2582004@gmail.com>
2024-09-22 11:53:49 +0300

    colors list view part2

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  final bool isActive;
+  final Color color;
+
+  const ColorItem({
+    required this.isActive,
+    required this.color,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 30,
-      backgroundColor: Colors.blue,
-    );
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 30,
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 30,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0XFF0A2463),
+    Color(0XFF3E92CC),
+    Color(0XFFFFFAFF),
+    Color(0XFFD8315B),
+    Color(0XFF1E1B18),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +52,16 @@ class ColorsListView extends StatelessWidget {
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return GestureDetector(
+            child: ColorItem(
+              isActive: currentIndex == index,
+              color: colors[index],
+            ),
+            onTap: () => setState(() => currentIndex = index),
+          );
         },
         separatorBuilder: (context, index) => const SizedBox(width: 8),
-        itemCount: 9,
+        itemCount: colors.length,
       ),
     );
   }
